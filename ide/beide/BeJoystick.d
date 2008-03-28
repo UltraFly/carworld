683ee233884f824616461e1f4e383f187a6b087b
hewat <hewat@f69ab8bf-2835-0410-8f28-9f2700608592>
2008-03-28 23:36:33 +0000

    deleted beide files
    git-svn-id: https://svn.code.sf.net/p/carworld/code/trunk@16 f69ab8bf-2835-0410-8f28-9f2700608592

--- ide/beide/BeJoystick.d
+++
@@ -1,5 +0,0 @@
-BeJoystick.o : /boot/home/work/carworld/src/beos/BeJoystick.cpp \
- /boot/home/work/carworld/src/beos/BeJoystick.h \
- /boot/home/work/carworld/src/beos/../H_Main.h \
- /boot/home/work/carworld/src/H_Keys.h \
- /boot/home/work/carworld/src/H_Standard.h
